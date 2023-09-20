9dabdc9ecd72273c27879b5dfebd40bcae159582
Lijazilla <alejandro-1986-jf@hotmail.com>
2023-09-19 21:39:34 -0600

    Se finalizó el diseño sencillo de una landing para box-braids, queda pendiente el responsive desing

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,40 +1,32 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import imagen1 from '../assets/images/results-client-3.jpeg'
-import imagen2 from '../assets/images/results-client-2.jpeg'
-import imagen3 from '../assets/images/results-client-4.jpeg'
-import imagen4 from '../assets/images/client-6.jpeg'
-import './carousel.css'
-
-
-
+    import React from 'react';
+    import Slider from 'react-slick';
+    import 'slick-carousel/slick/slick.css';
+    import 'slick-carousel/slick/slick-theme.css';
+    import './carousel.css'
 
 const Carousel = () => {
     const settings = {
-        fade: true, 
-        autoplay: true, 
+        fade: true,
+        autoplay: true,
         autoplaySpeed: 3000,
         speed: 600,
     };
-
+    
     return (
         <Slider {...settings}>
-            <div>
-                <img src={imagen1} alt="Imagen 1" className='first-image' />
+            <div className='carousel-div'>
+                <img src='https://i.imgur.com/rjw6pQP.jpg' alt="Imagen 1" />
             </div>
-            <div>
-                <img src={imagen2} alt="Imagen 2" className='second-image'/>
+            <div className='carousel-div'>
+                <img src='https://i.imgur.com/CJuZHvS.jpg' alt="Imagen 2" />
             </div>
-            <div>
-                <img src={imagen3} alt="Imagen 3" className='third-image'/>
-            </div>
-            <div>
-                <img src={imagen4} alt="Imagen 4" className='fourth-image'/>
+            <div className='carousel-div'>
+                <img src='https://i.imgur.com/e5DzdsI.jpg' alt="Imagen 3" />
             </div>
         </Slider>
     );
-};
+    };
 
-export default Carousel;
+    export default Carousel;
+
+    
